bbad072ee6f9fc0d9dd825eaf9003807fc1c4972
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-07-24 23:37:07 +0600

    add server domain name into axiospublic

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-  baseURL: 'http://localhost:5000', // Matches your Express server port
+  baseURL: import.meta.env.VITE_API_URL || "https://helix-diagnostic-server.vercel.app", // Matches your Express server port
 });
 
 const useAxiosPublic = () => {
