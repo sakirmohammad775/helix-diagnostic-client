714dad24e55b4c293e1e92ec80f384d64d1ea7c8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-07-24 23:18:16 +0600

    fixing frontend bug

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5173'
+    baseURL: import.meta.env.VITE_API_URL, // if you "http://localhost:5173/"
 })
 
 const useAxios = () => {
